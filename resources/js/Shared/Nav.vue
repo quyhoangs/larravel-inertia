83260c14c1a8ee7095fe89cd6354e158f7af8a74
quyhoangs <quyromutc@gmail.com>
2023-07-02 17:54:31 +0700

    Active Curent router Links

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,19 +1,30 @@
 <template>
-  <nav class="mt-6">
-    <ul class="list-disc">
-      <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-      <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-      <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-      <li>
-        <Link href="/logout"
-              method="POST"
-              as="button"
-              :data="{foo:'bar'}"
-              class="text-blue-500 hover:underline">Log Out
-        </Link>
-      </li>
-    </ul>
-  </nav>
+    <nav class="mt-6">
+        <ul class="list-disc">
+            <li>
+                <Link href="/" class="text-blue-500 hover:underline"
+                    :class="{ 'font-bold underline': $page.component === 'Home' }">
+                Home
+                </Link>
+            </li>
+            <li>
+                <Link href="/users?foo=bar" class="text-blue-500 hover:underline"
+                    :class="{ 'font-bold underline': $page.component === 'Users' }">
+                Users
+                </Link>
+            </li>
+            <li>
+                <Link href="/settings" class="text-blue-500 hover:underline"
+                    :class="{ 'font-bold underline': $page.component === 'Settings' }">Settings</Link>
+            </li>
+            <li>
+                <Link href="/logout" method="POST" as="button" :data="{ foo: 'bar' }" class="text-blue-500 hover:underline"
+                    :class="{ 'font-bold underline': $page.component === 'Logout' }">
+                Logout
+                </Link>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script>
