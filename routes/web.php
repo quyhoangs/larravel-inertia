d4b63df4b25c6c4640772749ad907b93e0f08149
quyhoangs <quyromutc@gmail.com>
2023-07-02 15:34:31 +0700

    Send request with method non-Get

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('/users', function () {
 Route::get('/settings', function () {
     return Inertia::render('Settings');
 });
+
+Route::post('/logout', function () {
+    dd(request('foo'));
+});
